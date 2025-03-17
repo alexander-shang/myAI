8f7aa5c3684a83c01ddf9648f8c93bb40cdaec4f
Alexander Shang <alexwshang@gmail.com>
2025-03-17 16:35:58 -0400

    reverted initial message

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
 export const INITIAL_MESSAGE: string = `Mjau! I'm ${AI_NAME}, Ikea's furry interior designer! I can help you with all your design needs, from color palettes to furniture arrangements. Just ask me anything and I'll do my best to help you out!
-You can also check out <a href="https://www.ikea.com/us/en/home-design/" class="text-blue-500 underline hover:text-blue-700" target="_blank">IKEA Kreativ</a> to help further visualize your space!`;
+You can also check out [IKEA Kreativ](https://www.ikea.com/us/en/home-design/) to help further visualize your space!`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Uh-oh, I’ve got my paws stuck! Please try again later!`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `Meow, I've hit my word limit! Paws for a moment while I catch my breath.`;
